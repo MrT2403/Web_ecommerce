58c9feeb58465c92012047ec2f912e4af509d04f
Kgreninja <79554625+Kgreninja@users.noreply.github.com>
2023-04-13 16:43:27 +0700

    .

---
+++ ReactJS/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
